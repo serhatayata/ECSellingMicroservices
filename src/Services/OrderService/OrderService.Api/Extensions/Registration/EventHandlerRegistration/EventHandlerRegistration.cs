54b101bfe51af116ace812933f3b659c97391a2c
Serhat <srhtayata@gmail.com>
2023-03-02 21:55:23 +0300

    OrderService.Api init

---
+++ src/Services/OrderService/OrderService.Api/Extensions/Registration/EventHandlerRegistration/EventHandlerRegistration.cs
@@ -0,0 +1,15 @@
+﻿using OrderService.Api.IntegrationEvents.EventHandlers;
+using OrderService.Api.IntegrationEvents.Events;
+
+namespace OrderService.Api.Extensions.Registration.EventHandlerRegistration
+{
+    public static class EventHandlerRegistration
+    {
+        public static IServiceCollection ConfigureEventHandlers(this IServiceCollection services)
+        {
+            services.AddTransient<OrderCreatedIntegrationEventHandler>();
+
+            return services;
+        }
+    }
+}
